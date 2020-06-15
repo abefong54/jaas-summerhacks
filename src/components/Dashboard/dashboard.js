f3539e940b1c843c1ae18bc577a29607cab11475
bestalmasever <almasfiza20@gmail.com>
2020-06-15 21:39:34 +0530

    Added an overall stats component using canvajs

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './dashboard.css';
+import Header from '/Users/almasfiza/Desktop/jaas-summerhacks/src/components/Dashboard/header/header.js';
+import OverallStats from './overallStats/OverallStats';
+import CanvasJSReact from '/Users/almasfiza/Desktop/jaas-summerhacks/src/canvas/canvasjs.react.js';
+//var CanvasJSReact = require('./canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 class dashboard extends React.Component{
     render(){
         return(
 
-            <h1>Dashboard</h1>           
+            <Header/> ,     
+            <OverallStats/>
         )
     }
 }
---
+++ src/components/Dashboard/overallStats/OverallStats.js
@@ -0,0 +1,60 @@
+
+import React from 'react';
+import CanvasJSReact from '/Users/almasfiza/Desktop/jaas-summerhacks/src/canvas/canvasjs.react.js';
+//var CanvasJSReact = require('./canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+
+
+
+class OverallStats extends React.Component{
+
+    render() {
+
+        
+        const options = {
+        backgroundColor: "black",
+        height:200,
+        width:400,
+        border:2,
+          title: {
+            text: "Overall stats"
+          },
+          axisX:{
+            interval: 1,
+            labelFontColor: "#87cefa",
+        },
+        axisY2:{
+            
+            gridColor: "black",
+            title: "Scale",
+            labelFontColor: "#87cefa",
+        },
+          data: [{				
+                    type: "bar",
+                    axisYType: "secondary",
+		color: "#87cefa",
+                    
+                    dataPoints: [
+                        { y: 200, label: "Surprised" },
+                        { y: 140, label: "Confused" },
+                        { y: 40, label: "Sad" },
+                        { y: 50, label: "Neutral" },
+                        { y: 120, label: "Happy"  },
+                    ]
+           }]
+       }
+            
+       return (
+          <div  className="overall">
+            <CanvasJSChart options = {options}
+                /* onRef = {ref => this.chart = ref} */
+            />
+          </div>
+        );
+      }
+
+
+}
+export default OverallStats;
