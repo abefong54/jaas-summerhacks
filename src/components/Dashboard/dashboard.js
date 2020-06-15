5635ebe724017c41158d9da65f0cb6ab2e7af7eb
bestalmasever <almasfiza20@gmail.com>
2020-06-15 09:40:47 +0530

    src folder--> login page

---
+++ src/components/Dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './dashboard.css';
+
+
+class dashboard extends React.Component{
+    render(){
+        return(
+
+            <h1>Dashboard</h1>           
+        )
+    }
+}
+
+export default dashboard;
