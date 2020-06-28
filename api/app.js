0f6a87b36bbbf5a8e712bf836a3caa00ddafe66d
Dion <53874292+dion-shushi@users.noreply.github.com>
2020-06-28 14:30:49 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -15,14 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/s3/sign', require('react-s3-uploader/s3router')({
-  accessKeyId: 'AKIA3ENAIKTK2KB2V5HQ', // optional you can set set your AWS_ACCESS_KEY_ID by ENV
-  secretAccessKey: 'ZPLFj9JDBXf+WQX6Ya9sRGanHur0VHQLW4JviS/E', // optional you can set your AWS_SECRET_ACCESS_KEY by ENV
-  bucket: "s3-bucket-v2",
-  signatureVersion: 'v2',
-  ACL: 'private', // this is default
-  uniquePrefix: true // (4.0.2 and above) default is true, setting the attribute to false preserves the original filename in S3
-}));
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
