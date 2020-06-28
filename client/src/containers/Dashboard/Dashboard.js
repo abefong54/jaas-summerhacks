89fd34bc24fcd83a4e76e5da2b38b8c1e3a69852
Dion <53874292+dion-shushi@users.noreply.github.com>
2020-06-28 15:50:12 -0500

    Update Dashboard.js

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -5,56 +5,56 @@ import ClassDropdown from '../../components/ClassDropdown/ClassDropdown';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles, withTheme } from '@material-ui/core/styles';
-import Upload from './Upload/upload';
+import Upload from '../../components/Upload/Upload';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const useStyles = makeStyles({
     root: {
     },
-    welcomeBanner:{
-        border:'5px solid lightskyblue',
+    welcomeBanner: {
+        border: '5px solid lightskyblue',
         borderRadius: '5px',
-        padding:'2em',
-        marginLeft:'2em',
-        marginBottom:'2em',
+        padding: '2em',
+        marginLeft: '2em',
+        marginBottom: '2em',
     },
-  });
+});
 var testProfessor = 'Apple';
 
-export default function Dashboard(){
-    const classes= useStyles();
+export default function Dashboard() {
+    const classes = useStyles();
 
-    return(
-<div style={{paddingTop:'100px' }}>
-                <Grid container spacing={12}>
-                    <Grid item xs={4}>
-                        <Paper className={classes.welcomeBanner} elevation={3}>
-                            <div> Welcome back, Professor {testProfessor}</div>
-                        </Paper> 
-                    </Grid>
-                    <Grid item xs={4}>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Grid container spacing={12}>
-                            <Grid item xs={4}>
-                                <ClassDropdown/>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <Upload />
-                            </Grid>
-                            <Grid item xs={4}>
-                                <Link to="/analytics" ><button>Take me to dashboard</button></Link>
-                            </Grid>
+    return (
+        <div style={{ paddingTop: '100px' }}>
+            <Grid container spacing={12}>
+                <Grid item xs={4}>
+                    <Paper className={classes.welcomeBanner} elevation={3}>
+                        <div> Welcome back, Professor {testProfessor}</div>
+                    </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                </Grid>
+                <Grid item xs={4}>
+                    <Grid container spacing={12}>
+                        <Grid item xs={4}>
+                            <ClassDropdown />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Upload />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Link to="/analytics" ><button>Take me to dashboard</button></Link>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <div style={{paddingLeft:'20px', paddingRight:'20px', paddingBottom:'20px',border:'2px solid lightskyblue',paddingBottom:'300px'}}>
-                            <h1>The Dashboard Body </h1>
-                            {/* CREATE DASHBOARD COMPONENT */}
-                        </div>
-                    </Grid>
                 </Grid>
-            </div>
+                <Grid item xs={12}>
+                    <div style={{ paddingLeft: '20px', paddingRight: '20px', paddingBottom: '20px', border: '2px solid lightskyblue', paddingBottom: '300px' }}>
+                        <h1>The Dashboard Body </h1>
+                        {/* CREATE DASHBOARD COMPONENT */}
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
 
     )
 }
