69c984f3429fa6dd5a39bd4294096407ba07b13b
Abraham Fong <abraham@parlevelsystems.com>
2020-06-28 03:52:06 -0400

    styling on dashboard

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './Dashboard.css';
 import ClassDropdown from '../../components/ClassDropdown/ClassDropdown';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { makeStyles, withTheme } from '@material-ui/core/styles';
 import Upload from './Upload/upload';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,33 +12,37 @@ const useStyles = makeStyles({
     root: {
     },
     welcomeBanner:{
-        paddingLeft:'30px',
         border:'5px solid lightskyblue',
         borderRadius: '5px',
-        padding:'20px',
+        padding:'2em',
+        marginLeft:'2em',
+        marginBottom:'2em',
     },
   });
+var testProfessor = 'Apple';
 
 export default function Dashboard(){
     const classes= useStyles();
 
     return(
 <div style={{paddingTop:'100px' }}>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <div className={classes.welcomeBanner}>
-                            Welcome back! -- Welcome Banner
-                        </div> 
+                <Grid container spacing={12}>
+                    <Grid item xs={4}>
+                        <Paper className={classes.welcomeBanner} elevation={3}>
+                            <div> Welcome back, Professor {testProfessor}</div>
+                        </Paper> 
                     </Grid>
-                    <Grid item xs={6}>
-                        <Grid container spacing={4}>
-                            <Grid item xs={2}>
+                    <Grid item xs={4}>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Grid container spacing={12}>
+                            <Grid item xs={4}>
                                 <ClassDropdown/>
                             </Grid>
-                            <Grid item xs={2}>
+                            <Grid item xs={4}>
                                 <Upload />
                             </Grid>
-                            <Grid item xs={2}>
+                            <Grid item xs={4}>
                                 <Link to="/analytics" ><button>Take me to dashboard</button></Link>
                             </Grid>
                         </Grid>
