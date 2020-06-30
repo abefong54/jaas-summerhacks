966e560dc16454f26c496684b0bf024df70dc1ef
bestalmasever <almasfiza20@gmail.com>
2020-06-30 07:23:05 +0530

    added back route button from analytics to dashboard

--- client/src/containers/Analytics/Analytics.js
+++ client/src/containers/Analytics/Analytics.js
@@ -1,90 +1,11 @@
-/* import React from 'react';
-import OverallStatsGraph from '../../components/OverallStatsGraph/OverallStatsGraph';
-import { makeStyles, withTheme } from '@material-ui/core/styles';
-import RealTimeStats from '../../components/RealTimeStats/RealTimeStats';
-import PlayVid from '../../components/PlayVid/PlayVid';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles({
-    root: {
-        marginTop: '50px',
-        padding:"30px"
-    },
-    vid:{
-        marginTop:"30px",
-        border:"1px solid lightskyblue",
-        padding:"150px",
-       
-        
-
-    },
-    realtime:{
-        marginTop:'10px',
-        border:"1px solid lightskyblue",
-        padding:"100px",
-        marginBottom:'10px',
-        
-
-    },
-    backbutton:{
-      border: 0,
-      borderRadius: 5,
-      backgroundColor:"black",
-      color:"white",
-     
-      height: 48,
-      padding: '0 30px',
-      weight: 'bold',
-      marginTop:"70px",
-      marginLeft:"30px"
-
-    }
-    
-    
-      
-    
-    
-
-  });
-
-export default function Analytics(){
-    const style=useStyles();
-    return(
-        <div>
-        
-   <Link to="/dashboard" ><button className={style.backbutton}>Back</button></Link>
-
-        
-
-        <div className={style.root}>
-            <div className={style.vid}>
-                <PlayVid/>
-            </div>
-            <div className={style.realtime}>
-                <RealTimeStats/>
-
-            </div>
-            <div className={style.overall}>
-            <OverallStatsGraph/>
-
-             </div>
-             </div>
-        
-        
-        
-        </div>
-        
-    )
-
-} */
  
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import OverallStatsGraph from '../../components/OverallStatsGraph/OverallStatsGraph';
-
+import { Link } from 'react-router-dom';
 
 
 import { Component } from 'react'
@@ -99,6 +20,16 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: theme.palette.text.secondary,
   },
+  backbutton:{
+    background: 'black',
+    border: 0,
+    borderRadius: 5,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color:'#87cefa' ,
+    height: 48,
+    padding: '0 30px',
+    weight: 'bold',
+  }
 }));
 
 export default function CenteredGrid() {
@@ -107,6 +38,8 @@ export default function CenteredGrid() {
   return (
     <div style={{padding: '2% 2%'}, {margin: '5% 2% 2% 2%'}}>
       <div className={classes.root}>
+      <Link to="/dashboard" ><button className={classes.backbutton}>BACK</button></Link>
+                           
         <Grid container spacing={3}>
           <Grid item xs={4}>
             <div style={{ maxWidth: '100%'}}>
